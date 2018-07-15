f92ac24741b501f80cc72a334007d54c55d65220
anetegithub <anete.anetes@gmail.com>
2018-07-15 21:54:30 +0300

    Add project files.

---
+++ Linq/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //var db = new Data
+            //{
+            //    Title="Root",
+            //    Grade=0,
+            //    Obj=new List<>
+            //}
+
+            //var query = new 
+        }
+    }
+
+    public class NuGetFeed<TNuGetPackage> : IQueryable<TNuGetPackage> where TNuGetPackage : INuGetPackage
+    {
+        public Type ElementType => throw new NotImplementedException();
+
+        public Expression Expression => throw new NotImplementedException();
+
+        public IQueryProvider Provider => throw new NotImplementedException();
+
+        public IEnumerator<TNuGetPackage> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class NuGetQueryProvider : IQueryProvider
+    {
+        public IQueryable CreateQuery(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Execute(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface INuGetPackage
+    {
+
+    }
+}
